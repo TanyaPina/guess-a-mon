ef3d9868423b6ceb2a4cddac0fd28a794538f255
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-04 11:32:50 -0700

    added home page background image

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-
 const Home = () => {
 
     return (
-  <div>Hello</div>
+  <div 
+    style={{ 
+        backgroundImage: "url('http://localhost:5173/homeback.png')", 
+        height:"100vh",
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat', }}>Hello</div>
     );
 };
 
--- client/src/components/pages/Root.jsx
+++ client/src/components/pages/Root.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MyNavBar from "../Navbar";
 import { Outlet } from "react-router-dom";
+import Home from "./Home";
 
 export default function Root(){
     return(
         <div className="App">
         <MyNavBar/>
+        <Home/>
         <Outlet/>
         </div>
     );
