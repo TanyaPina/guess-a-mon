ffe1abd85258670bc476ff54893bf89348bb2d06
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-03 21:19:21 -0700

    added root file for react router functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,23 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from './components/Navbar'
 import ListStudents from './components/ListStudents'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './components/pages/Root';
 import Home from './components/pages/Home';
 
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Root />,
+},
+]);
+
+
+function App(){
+  return <RouterProvider router={router}/>;
+}
+/*
 function App() {
 
   return (
@@ -15,5 +29,6 @@ function App() {
     </div>
   )
 }
+*/
 
 export default App
