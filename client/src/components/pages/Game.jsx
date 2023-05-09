1378faab5c72d13aca7b46c18f28ae94bf63934a
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-09 14:40:31 -0700

    added loadPokemon function

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Game = () => {
 
+    const [pokemon, setPokemon] = useState([]);
 
+    const loadPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        .then((response) => response.json())
+        .then((pokemon) => {
+            setPokemon(pokemon);
+        });
+    }
+
+     return (
+       <div className="mybody">
+        <h2>Hello</h2>
+       </div>
+     );
 }
 
 
