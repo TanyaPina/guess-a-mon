8a5c1f5357806a17277c40331315969a10f97910
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-10 10:55:38 -0700

    added ditto mock data

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import dittoJson from "../../sampleresponse/ditto.json"
 
 const Game = () => {
 
-    const [pokemon, setPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState(dittoJson);
 
-    const loadPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-            .then((response) => response.json())
-            .then((pokemon) => {
-                setPokemon(pokemon);
-            });
-    }
+    // const loadPokemon = () => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    //         .then((response) => response.json())
+    //         .then((pokemon) => {
+    //             setPokemon(pokemon);
+    //         });
+    // }
 
-    useEffect(() => {
-        loadPokemon();
-    }, []);
+    // useEffect(() => {
+    //     loadPokemon();
+    // }, []);
+
+
     return (
         <div className="mybody">
-            <h2>Hello</h2>
+            <div className="poketainer">
+                <h2>Pokemon</h2>
+                <p>The name of the pokemon is {pokemon.name}</p>
+            </div>
         </div>
     );
 }
--- client/src/components/pages/Root.jsx
+++ client/src/components/pages/Root.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MyNavBar from "../Navbar";
-
+import Game from "./Game";
 export default function Root(){
     return(
         <div className="App">
         <MyNavBar/>
+        <Game/>
         </div>
     );
 };
