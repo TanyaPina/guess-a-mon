b2d40da9507ff0483a1953bbf39def5b3c508c55
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-16 02:13:10 -0700

    added shuffle pokemon functionality

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,29 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import dittoJson from "../../sampleresponse/ditto.json"
 import GameContainer from './GameContainer';
+import firstfifteenJson from "../../sampleresponse/firstfifteen.json"
 
 const Game = () => {
 
-    const [pokemon, setPokemon] = useState(dittoJson);
+    //const [pokemon, setPokemon] = useState(dittoJson);
+    const [pokemonList, setPokemonList] = useState([]);
+    //const [isLoading, setIsLoading] = useState(false);
+    
+  
+    const getPokemonList = () => {
+        console.log("calling api")
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+            .then((response) => response.json())
+            .then((pokemonListResponse) => {
+                console.log("calling api response", pokemonListResponse)
+                shuffledPokemonList(pokemonListResponse.results);
+            });
+    }
 
-    // const loadPokemon = () => {
-    //     fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    //         .then((response) => response.json())
-    //         .then((pokemon) => {
-    //             setPokemon(pokemon);
-    //         });
-    // }
+    const shuffledPokemonList = (pokemonListParam) => {
+        //setIsLoading(true);
+        const shuffled = pokemonListParam
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value);
+        setPokemonList(shuffled);
+        getFourPokemon(shuffled);
+        //setIsLoading(true);
+        console.log("line34", pokemonList);
+        //setIsLoading(false);
+    }
 
-    // useEffect(() => {
-    //     loadPokemon();
-    // }, []);
+
+    useEffect(() => {
+        getPokemonList();
+        // shuffledPokemonList();
+        console.log("line40", pokemonList);
+    }, []);
 
 
     return (
         <div className="mybody">
             <div className="poketainer">
-                <h2>Pokemon {pokemon.name}</h2>
-                <GameContainer pokemon={pokemon}/>
+                {/* <h2>Pokemon {pokemon.name}</h2>
+                // <GameContainer pokemon={pokemon}/> */}
+                     {/* <p>Hello this is my pokemon {pokemonList[0]?.name}</p> */}
+                     { pokemonList.length == 0 ? null : <p>Hello this is my pokemon {pokemonList[0].name} </p> }
+                     <GameContainer pokemonList={pokemonList} shuffledPokemonList={shuffledPokemonList}/>
             </div>
         </div>
     );
--- client/src/components/pages/GameContainer.jsx
+++ client/src/components/pages/GameContainer.jsx
@@ -1,21 +1,49 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import { Button, Container, Header } from 'semantic-ui-react'
 
-const GameContainer = ({pokemon}) => {
+const GameContainer = ({pokemonList, shuffledPokemonList}) => {
 
+    // const [fourPokemon, setFourPokemon] = useState();
+    let fourPokemon = [];
     // const pokemonSprite = (pokemon) =>{
     //     return '{pokemon.pokemon.sprites.back_default}'
     // }
 
+
+
+    if (pokemonList.length == 0) return null ;
+
+    const getFourPokemon = (shuffled) => {
+        // const fourPokemonResult = shuffled.splice(0,4);
+        // setFourPokemon(fourPokemonResult);
+        fourPokemon = shuffled.splice(0,4);
+    }
+
+    getFourPokemon(pokemonList);
+
+    const correctOption = fourPokemon[Math.floor(Math.random() * fourPokemon.length)];
+
+    // const shuffledPokemonList = (pokemonListParam) => {
+    //     //setIsLoading(true);
+    //     const shuffled = pokemonListParam
+    //         .map(value => ({ value, sort: Math.random() }))
+    //         .sort((a, b) => a.sort - b.sort)
+    //         .map(({ value }) => value);
 
     return (
         <Container textAlign='center'
         style={{
             backgroundColor: "white",
         }}>
-           <p>Hello! The pokemon is {pokemon.name}</p>
-           <img src={pokemon.sprites.front_default}/>
-           
+           {/* <p>Hello! The pokemon is {pokemon.name}</p> */}
+           {/* { pokemonList.length == 0 ? null : <p>Hello this is my pokemon {pokemonList[0].name} </p> } */}
+           {/* <img src={pokemonList.sprites.front_default}/> */}
+           <p><Button onClick={()=>{shuffledPokemonList(pokemonList)}}> Shuffle </Button></p>
+           <Button onClick={()=>{shuffledPokemonList(pokemonList)}}> {fourPokemon[0].name} </Button>
+           <Button onClick={()=>{shuffledPokemonList(pokemonList)}}> {fourPokemon[1].name} </Button>
+           <Button onClick={()=>{shuffledPokemonList(pokemonList)}}> {fourPokemon[2].name}</Button>
+           <Button onClick={()=>{shuffledPokemonList(pokemonList)}}> {fourPokemon[3].name}</Button>
+           <p>Hello! The correct pokemon is {correctOption.name}</p>
         </Container>
     );
 }
