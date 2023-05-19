8d659bcb817dbe6ca5b8de27fa27a3679ada3bb8
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-19 15:21:41 -0700

    added tests for the page components

--- client/src/components.test.jsx
+++ client/src/components.test.jsx
@@ -1,26 +1,40 @@
 import Root from './components/pages/Root';
 import Home from './components/pages/Home';
 import Game from './components/pages/Game';
+import GameContainer from './components/pages/GameContainer';
 import MyNavBar from './components/Navbar';
 import {expect, test} from 'vitest';
 import {render, screen} from '@testing-library/react';
-
-
-//tests if root renders - not working because of {user.name} being undefined
+ 
+//TODO: tests if root renders - not working because of {user.name} being undefined
 // test('Root renders correctly', () => {
 //   render(<Root />);
 //   expect(<Root/>).toBeDefined();
 // });
+
+//TODO: tests if root renders - not working because of {user.name} being undefined
+// test('NavBar renders correctly', () => {
+//   render(<MyNavBar />);
+//   expect(<MyNavBar/>).toBeDefined();
+// });
+
 
 test('Home renders correctly', () => {
   render(<Home />);
   expect(<Home />).toBeDefined();
 });
 
+
 test('Game renders correctly', () => {
   render(<Game />);
   expect(<Game/>).toBeDefined();
 });
+
+//TODO: tests if GameContainer renders - failed because pokemonList is not iterable
+// test('GameContainer renders correctly', () => {
+//   render(<GameContainer />);
+//   expect(<GameContainer/>).toBeDefined();
+// });
 
 test('Check for login button in home', () => {
     render(<Home/>);
