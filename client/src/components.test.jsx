5cf7e6ccafbca6e9250d5426ec6732b5d096ea46
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-19 15:01:59 -0700

    added tests for the home component

---
+++ client/src/components.test.jsx
@@ -0,0 +1,22 @@
+import Root from './components/pages/Root';
+import Home from './components/pages/Home';
+import MyNavBar from './components/Navbar';
+import {expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+
+//tests if root renders - not working because of {user.name} being undefined
+// test('Root renders correctly', () => {
+//   render(<Root />);
+//   expect(<Root/>).toBeDefined();
+// });
+
+test('Home renders correctly', () => {
+  render(<Home />);
+  expect(<Home />).toBeDefined();
+});
+
+test('Check for login button in home', () => {
+    render(<Home/>);
+    const button = screen.getByText('Log In');
+    expect(button).toBeDefined();
+});
