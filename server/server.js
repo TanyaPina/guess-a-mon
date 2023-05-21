443bec8415a2b53a06005ea7121e7c6f40bc6a33
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-20 17:34:20 -0700

    added post request for users

--- server/server.js
+++ server/server.js
@@ -16,11 +16,23 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(REACT_BUILD_DIR, "index.html"));
 });
 
-// create the get request for students in the endpoint '/api/students'
-app.get('/api/students', async (req, res) => {
+// create the post request for users in the endpoint '/api/users'
+app.post('/api/users', async (req, res) => {
     try {
+        const userAccount = req.body.user;
+        const userEmail = await db.query("SELECT * from users where email = $1", [userAccount.email,])
         const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
+        if (userEmail.rows.length === 0){
+            const newUser = {
+                username : req.body.user.name,
+                email: req.body.user.email,
+            };
+            const result = await db.query("INSERT INTO users(username, email) VALUES ($1,$2) RETURNING *",
+            [newUser.name, newUser.email]
+          );
+        }
+        console.log("line34", result.rows[0]);
+        res.json(result.rows[0]);
     } catch (e) {
         return res.status(400).json({ e });
     }
