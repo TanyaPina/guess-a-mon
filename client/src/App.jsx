d0814bff2f1dc4165fe3cfbc181e06d44dcb2481
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-21 16:35:23 -0700

    added user post to server and state to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,25 +6,29 @@ import Home from './components/pages/Home';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from "react-router-dom"; 
 import Game from './components/pages/Game';
+import { useState } from 'react';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path="/" element={<Root />}>
-    <Route index element={<Game/>} />
-    <Route path="favorites" element={<div>Favorites</div>}/>
-    </Route>
-  )
-  )
+
 
 function App() {
 
-  const { isAuthenticated, isLoading} = useAuth0();
+  const { isAuthenticated, isLoading, user} = useAuth0();
+  const [userObj, setUserObj] = useState(null);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Game/>} />
+      <Route path="favorites" element={<div>Favorites</div>}/>
+      </Route>
+    )
+    )
 
   if (isLoading) return <div>Loading...</div>
   
   return (
     <div className="App">
-      {!isAuthenticated ? <Home/> : <RouterProvider router={router}/>}      
+      {!isAuthenticated ? <Home setUserObj={setUserObj}/> : <RouterProvider router={router}/>}      
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -16,15 +16,57 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(REACT_BUILD_DIR, "index.html"));
 });
 
-// create the get request for students in the endpoint '/api/students'
-app.get('/api/students', async (req, res) => {
+app.get("/api/users", cors (), async (req, res) => {
     try {
-        const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
+        const { rows: users } = await db.query("SELECT * FROM users");
+      res.send(users);
+      console.log("line23server")
+    } catch (e) {
+      return res.status(400).json({ e });
+    }
+  });
+
+  app.get("/api/favorites", cors() , async (req, res) => {
+    try {
+      const { rows: favorites } = await db.query("SELECT * FROM favorites");
+      res.send(favorites);
+      console.log("line34server")
+    } catch (e) {
+      return res.status(400).json({ e });
+    }
+  });
+
+// create the post request for users in the endpoint '/api/users'
+app.post('/api/users', async (req, res) => {
+    try {
+        const userAccount = req.body.user;
+        const userEmail = await db.query("SELECT * from users where email = $1", [userAccount.email,])
+        if (userEmail.rows.length === 0){
+            const newUser = {
+                username : req.body.user.name,
+                email: req.body.user.email,
+            };
+            const result = await db.query("INSERT INTO users(username, email) VALUES ($1,$2) RETURNING *",
+            [newUser.name, newUser.email]
+          );
+        }
+        console.log("line34", result.rows[0]);
+        res.json(result.rows[0]);
     } catch (e) {
         return res.status(400).json({ e });
     }
 });
+// create the get request for students in the endpoint '/api/students'
+// app.get('/api/students', async (req, res) => {
+//     try {
+//         const { rows: students } = await db.query('SELECT * FROM students');
+//         res.send(students);
+//     } catch (e) {
+//         return res.status(400).json({ e });
+//     }
+// });
+
+
 
 // create the POST request
 app.post('/api/students', async (req, res) => {
