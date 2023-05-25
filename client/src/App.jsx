335a772768244ade60e120b037390d92665328bb
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-25 04:58:09 -0700

    cleaned navbar.jsx code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 import Root from './components/pages/Root';
 import Home from './components/pages/Home';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from "react-router-dom"; 
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Game from './components/pages/Game';
 import ListFavorites from './components/pages/ListFavorites';
 import { useEffect, useState } from 'react';
@@ -17,10 +17,10 @@ function App() {
   const [userObj, setUserObj] = useState(null);
   const [userFavorites, setUserFavorites] = useState([]);
 
-//loads favorites if user exists
+  //loads favorites of user 
   const loadFavorites = async () => {
     if (userObj && userObj.email) {
-      fetch(`/api/user/favorites/${userObj.email}`) 
+      fetch(`/api/user/favorites/${userObj.email}`)
         .then((response) => response.json())
         .then((data) => {
           setUserFavorites(data);
@@ -29,23 +29,23 @@ function App() {
   };
 
   useEffect(() => {
-    if (userObj) loadFavorites(); 
-  }, [userObj]); 
+    if (userObj) loadFavorites();
+  }, [userObj]);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-    <Route path="/" element={<Root setUserObj={setUserObj}/>}>
-      <Route index element={<Game/>} />
-      <Route path="favorites" element={<ListFavorites  setUserFavorites={setUserFavorites} userFavorites={userFavorites}/>}/>
+      <Route path="/" element={<Root setUserObj={setUserObj} />}>
+        <Route index element={<Game />} />
+        <Route path="favorites" element={<ListFavorites setUserFavorites={setUserFavorites} userFavorites={userFavorites} />} />
       </Route>
     )
-    )
+  )
 
   if (isLoading) return <div>Loading...</div>
-  
+
   return (
     <div className="App">
-      {!isAuthenticated ? <Home /> : <RouterProvider router={router}/>}      
+      {!isAuthenticated ? <Home /> : <RouterProvider router={router} />}
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,33 +1,32 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Logo from '../assets/BlueTechtonicaWord.png'
 import { Button } from 'semantic-ui-react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function MyNavBar(props) {
 
-  const { logout, isAuthenticated, user} = useAuth0();
-  
+  const { logout, isAuthenticated, user } = useAuth0();
+
   return (
     <>
-    <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-        <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="favorites">Favorites</Nav.Link>
-          </Nav>
-          <Navbar.Text>
-             <a href="#login" style={{marginRight: "20px"}}>Welcome, {user.name}</a>
-            {isAuthenticated && (<Button inverted onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-              Log Out
+      <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="favorites">Favorites</Nav.Link>
+            </Nav>
+            <Navbar.Text>
+              <a href="#login" style={{ marginRight: "20px" }}>Welcome, {user.name}</a>
+              {isAuthenticated && (<Button inverted onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                Log Out
               </Button>)}
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
