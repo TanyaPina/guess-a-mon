892f1d5a0045d9481ba8e6865d61c64470e96846
Tanya Piña <tanyapina@Tanyas-MBP.lan>
2023-05-25 05:01:03 -0700

    cleaned favoritecard.jsx code

--- client/src/components/FavoriteCard.jsx
+++ client/src/components/FavoriteCard.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import * as ioicons from 'react-icons/io5'
 
-const FavoriteCard = ({favorite}) => {
+const FavoriteCard = ({ favorite }) => {
 
     const onUpdate = (toUpdateStudent) => {
         toUpdate(toUpdateStudent)
@@ -16,9 +14,7 @@ const FavoriteCard = ({favorite}) => {
     return (
         <Card>
             <Card.Body>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${favorite.pokecode}.png`}/>
-            {/* <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
-            <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button> */}
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${favorite.pokecode}.png`} />
             </Card.Body>
         </Card>
     )
